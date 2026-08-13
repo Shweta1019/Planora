818d007f940ca972e6e3f6c4ecd7ddf007e3f9de
AJain921 <jainakshita219@gmail.com>
2026-08-14 00:25:35 +0530

    updated axios baseURL for deployment

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 import { useAuthStore } from '../store/authStore'
 
 const api = axios.create({
-  baseURL: '/api',
+  // baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
   timeout: 15000,
   headers: { 'Content-Type': 'application/json' },
 })
